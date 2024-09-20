c6545665ef48d1642e0eaae23c720529cb5c79c0
David Eriksson <david.eriksson2@elev.ga.ntig.se>
2024-09-20 10:55:30 +0200

    m

--- Slagsmålsspelet/Program.cs
+++ Slagsmålsspelet/Program.cs
@@ -1,3 +1,112 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System.Security.Cryptography;
+using SC = System.Console;
+
+//
+// Slumpstiden V.1 som kommer bara vara på en linije!!
+//
+
+while /*MAIN*/ (true) {
+
+void NAME() { /*Tar in spelarens namn*/
+SC.WriteLine("Skriv dit namn:");
+string name = SC.ReadLine();
+while (name.Length < 3) {SC.Clear(); SC.WriteLine("Skriv dit namn:"); name = SC.ReadLine();}
+SC.ReadLine();
+}
+
+void APONENT(){ /*Inehåller motståndare och ger ut värden för dem */
+
+
+
+}
+
+
+void FIGHT() {  /* Utför slump striden */
+SC.WriteLine("Fight Begin!");
+
+
+}
+
+void WIN() {}
+void LOSE(){}
+void RESTART(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} /* WHILE MAIN /*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+while (true){
+int card = Random.Shared.Next(1, 53); // inclusivt golv, exlusivt tak. kommer ge 1 men kommer inte ge 53 utan en lägre
+SC.WriteLine(card);
+SC.ReadLine();
+
+
+SC.WriteLine("Skriv dit namn:");    string name = SC.ReadLine();
+while (name.Length < 3) {SC.Clear(); SC.WriteLine("skriv in dit namn namn sa jag dumhuvve"); name = SC.ReadLine();}
+SC.ReadLine();
+}
+*/
