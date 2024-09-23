828d27c86a02310ff92e452ec2a51eef9741dbe5
David Eriksson <david.eriksson2@elev.ga.ntig.se>
2024-09-23 09:36:20 +0200

    mmm

--- Slagsmålsspelet/Program.cs
+++ Slagsmålsspelet/Program.cs
@@ -6,6 +6,24 @@ using SC = System.Console;
 //
 
 bool ChosingAponent = true;
+bool Aponent_Punch = false;
+bool Aponent_kick = false;
+bool Aponent_Block = false;
+bool Aponent_Pary = false;
+
+bool Fighting = true;
+
+/* Vall under striden */
+bool fight_Punch = false;
+bool fight_kick = false;
+bool fight_block = false;
+bool fight_pary = false;
+bool fight_Consed = false;
+/* ------------------------ */
+
+int Player_Helth;
+int Aponent_Helth;
+int Damage;
 
 
 while /*MAIN*/ (true) {
@@ -42,13 +60,95 @@ while (ChosingAponent == true) {
 SC.WriteLine("Chose your oponent:");
 SC.WriteLine("Aponent List whit case sats");
 string Aponent_id = SC.ReadLine();
+switch ((string)(Aponent_id)) {
+    case (string)("1"):
+    ChosingAponent = false;
+    SC.WriteLine("You have chosen aponent one.");
+    SC.WriteLine("ASCI & Stats for aponetn Placeholder");
+    SC.ReadLine();
+    break;
+    case (string)("2"):
+    ChosingAponent = false;
+    SC.WriteLine("You have chosen aponent two.");
+    SC.WriteLine("ASCI & Stats for aponetn Placeholder");
+    SC.ReadLine();
+    break;
+    case (string)("3"):
+    ChosingAponent = false;
+    SC.WriteLine("You have chosen aponent tre.");
+    SC.WriteLine("ASCI & Stats for aponetn Placeholder");
+    SC.ReadLine();
+    break;
+}
                                 }
+    SC.WriteLine("press any key to continue");
 }
 
 
 void FIGHT() {  /* Utför slump striden */
-SC.WriteLine("Fight Begin!");
-}
+SC.WriteLine("Let the fight Begin!");
+SC.WriteLine("ASCI");
+SC.WriteLine("Trash talk");
+SC.ReadLine();
+SC.Clear();
+
+
+while (Fighting) { // Lopar tills Slagsmålet är över
+SC.WriteLine("ASCI");
+SC.WriteLine("Fight Choices");
+Damage = 0;
+string Fight_Choice = SC.ReadLine();
+switch ((string)(Fight_Choice)){
+    case (string)("1"):
+    fight_Punch = true;
+    Damage = Random.Shared.Next(20, 44);
+    if (Aponent_Block) {
+        Damage = Random.Shared.Next(5, 17);
+    } else if (Aponent_Pary) {
+        Damage = Random.Shared.Next(3, 7);
+        Player_Helth =- Random.Shared.Next(15, 33);
+    }
+    Aponent_Helth =- Damage; 
+    break;
+    case (string)("2"):
+    fight_kick = true;
+        Damage = Random.Shared.Next(30, 50);
+    if (Aponent_Block) {
+        Damage = Random.Shared.Next(10, 30);
+    } else if (Aponent_Pary) {
+        Damage = Random.Shared.Next(1, 15);
+        Player_Helth =- Random.Shared.Next(30, 45);
+    }
+    Aponent_Helth =- Damage; 
+    break;
+    case (string)("3"):
+    fight_block = true;
+    if (Aponent_Punch || Aponent_kick) {
+       Player_Helth =- Random.Shared.Next(5, 30); 
+    }
+    break;
+    case (string)("4"):
+    fight_pary = true;
+    if (Aponent_Punch || Aponent_kick) {
+        Player_Helth =- Random.Shared.Next(5, 30);
+        Damage = Random.Shared.Next(30, 45);
+        Aponent_Helth =- Damage; 
+    }
+    break;
+    case (string)("5"):
+    fight_Consed = true;
+    Fighting = false;
+    break;
+    case (string)("6"):
+    Fight_TrashTalk();
+    break;
+} //End case
+
+} /* While Fhigting*/
+
+} 
+
+void Fight_TrashTalk() {}
 
 void RESULT() {}
 
